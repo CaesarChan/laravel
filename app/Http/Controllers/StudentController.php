d21223bc39aa53f7348cadb2f709cacf60240e4d
caesar chan <caesar_chan@live.com>
2017-03-10 19:49:18 +0800

    no message

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/3/10
+ * Time: 8:01
+ */
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Support\Facades\DB;
+
+class StudentController extends Controller
+{
+    public function index(){
+
+        return 'Hello World';
+    }
+}
